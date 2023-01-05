539265861b20536e9cbc783ae3f24e3e67654bd1
darskan <darskan243@gmail.com>
2023-01-04 19:42:06 -0500

    menu

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import 'semantic-ui-css/semantic.min.css'
 import './App.css';
-import Comics from './pages/Comics';
-import Error404 from './pages/Error404';
-import Home from './pages/Home';
-import Series from './pages/Series';
+import Header from    './components/Header';
+import Comics from    './pages/Comics';
+import Error404 from  './pages/Error404';
+import Home from      './pages/Home';
+import Series from    './pages/Series';
 
 
 function App() {
   return (
     <div>
       <BrowserRouter>
+        <Header/>
         <Routes>
           <Route path="/" element={<Home />        } />
           <Route path="/inicio" element={<Home/>   } />
           <Route path="/series" element={<Series/> } />
-          <Route path="/comics" element={<Comics/> } />
+          <Route path="/comics" element={<Comics/> } />            
           <Route path="*" element={<Error404/> } />
         </Routes>
       </BrowserRouter>
